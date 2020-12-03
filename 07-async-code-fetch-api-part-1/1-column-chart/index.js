494966ca91be2df280560307fd0b1f302b547bba
Ivan <astaninivan13@gmail.com>
2020-12-03 16:17:45 +0300

    column chart fetch

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,12 +1,16 @@
+import fetchJson from './utils/fetch-json.js';
+
 export default class ColumnChart {
+  data = {}
   element = {};
+  subElements = {};
   chartHeight = 50;
-  data = [];
-  constructor({url = '', range = {from: new Date(), to: new Date()}, label = '', link = ''} = {}) {
+  constructor({url = '', range = {from: new Date(), to: new Date()}, label = '', link = '', formatHeading = data => data} = {}) {
     this.url = url;
     this.range = range;
     this.label = label;
     this.link = link;
+    this.formatHeading = formatHeading;
     this.render();
   }
 
@@ -14,26 +18,40 @@ export default class ColumnChart {
     const element = document.createElement('div');
     element.innerHTML = this.chart;
     this.element = element.firstElementChild;
+    this.getChartElements();
+    this.updateChartValues();
   }
 
-  async update(begin, end) {
-    this.data = await this.getData(begin, end);
-    const columns = this.element.querySelector('.column-chart__chart');
-    // columns.innerHTML = this.columns;
+  async update(dateFrom, dateTo) {
+    this.range.from = dateFrom;
+    this.range.to = dateTo;
+    await this.updateChartValues();
   }
 
-  async getData(begin = new Date(), end = new Date()) {
-    try {
-      const url = `https://course-js.javascript.ru/${this.url}?from=${begin.toJSON()}&to=${end.toJSON()}`;
-      return await fetch(url).then(response => response.json());
-    } catch (err) {
-      console.error(err);
-      return [];
+  async updateChartValues() {
+    this.element.classList.add('column-chart_loading');
+    const url = `https://course-js.javascript.ru/${this.url}?from=${this.range.from.toJSON()}&to=${this.range.to.toJSON()}`;
+    this.data = await fetchJson(url);
+    if (this.data && Object.values(this.data).length) {
+      this.subElements.header.textContent = this.totalValue;
+      this.subElements.body.innerHTML = this.columns;
+      this.element.classList.remove('column-chart_loading');
     }
+    console.log(this.subElements.body.children.length, Object.values(this.data).length)
+  }
+
+  getChartElements() {
+    const elements = this.element.querySelectorAll('[data-element]');
+    this.subElements = [...elements].reduce((accum, element) => {
+      accum[element.dataset.element] = element;
+      return accum;
+    }, {});
   }
 
   remove() {
-    this.element.remove();
+    if (this.element) {
+      this.element.remove();
+    }
   }
 
   destroy() {
@@ -43,11 +61,10 @@ export default class ColumnChart {
 
   get chart() {
     return `
-      <div class="column-chart ${!this.data.length ? 'column-chart_loading' : ''}"  style="--chart-height: ${this.chartHeight}">
+      <div class="column-chart column-chart_loading"  style="--chart-height: ${this.chartHeight}">
         ${this.title}
         <div class="column-chart__container">
           <div data-element="header" class="column-chart__header">
-             ${this.totalValue}
           </div>
           <div data-element="body" class="column-chart__chart" >
           </div>
@@ -72,15 +89,15 @@ export default class ColumnChart {
   }
 
   get totalValue() {
-    return this.data.reduce((accum, value) => accum += value, 0);
+    return this.formatHeading(Object.values(this.data).reduce((accum, value) => accum += value, 0));
   }
 
   get columns() {
-    const max = Math.max(...this.data);
+    const max = Math.max(...Object.values(this.data));
     const scale = this.chartHeight / max;
-    return [...this.data].map(x => {
-      const percent = (x / max * 100).toFixed(0);
-      return `<div style="--value: ${Math.floor(x * scale)}" data-tooltip="${percent}%"></div>`;
+    return Object.entries(this.data).map(([key, val]) => {
+      const percent = (val / max * 100).toFixed(0);
+        return `<div style="--value: ${Math.floor(val * scale)}" data-tooltip="${percent}%"></div>`;
     }).join('');
   }
 }
